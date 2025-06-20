5ca8a426036d0b7bfe5c943352904c6837c71093
dismad <81990132+dismad@users.noreply.github.com>
2025-06-20 11:52:48 -0700

    Update main.rs
    Moved init() function into the NodeConnection struct, removed multiple hard coded urls into a single entry in strut

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use dialoguer::Select;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{Read, Result, Write};
+use std::path::Path;
 use std::process;
 use std::process::{Command, Stdio};
-use std::path::Path;
 //use clap::{command, Arg};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -33,77 +33,85 @@ struct NodeData {
     monitored: bool,
 }
 
-struct NodeConnection {
+pub struct NodeConnection {
     url: String,
     port: i32,
 }
+
+impl NodeConnection {
+    fn display(&self) -> String {
+        self.url.clone() + ":" + &self.port.to_string()
+    }
+}
+
+impl NodeConnection {
+    fn init(&self) -> String {
+        println!(
+            "\nAttempting to connect to Zebrad @ {}:{}\n.\n.\n.",
+            self.url, self.port
+        );
+        let mymethod = "getinfo";
+        let body_string = format!(
+            "{{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"{}\", \"params\": []}}",
+            mymethod
+        );
+        let mut body = body_string.as_bytes();
+
+        let mut easy = Easy::new();
+        easy.url(&self.display()).unwrap();
+        easy.post(true).unwrap();
+        easy.post_field_size(body.len() as u64).unwrap();
+
+        // Set the Content-Type header to application/json
+        let mut list = curl::easy::List::new();
+        list.append("Content-Type: application/json").unwrap();
+        easy.http_headers(list).unwrap();
+
+        let mut data = Vec::new();
+        {
+            // Create transfer in separate scope ...
+            let mut transfer = easy.transfer();
+
+            // Request body
+            transfer
+                .read_function(|buf| Ok(body.read(buf).unwrap_or(0)))
+                .unwrap();
+
+            // Response body
+            transfer
+                .write_function(|new_data| {
+                    data.extend_from_slice(new_data);
+                    Ok(new_data.len())
+                })
+                .unwrap();
+
+            transfer.perform().expect("Could not connect!\n")
+            // .. to force drop it here, so we can use easy.response_code()
+        }
+
+        let check = easy.response_code().unwrap().to_string();
+
+        if check == "200" {
+            println!("Connected!");
+        } else {
+            println!("No response!");
+        }
+
+        self.display()
+    }
+}
+
 fn main() {
     let my_connection = NodeConnection {
         url: String::from("http://127.0.0.1"),
         port: 8232,
     };
+    let myserver = my_connection.init();
 
-    init(my_connection);
-    display_menu().unwrap();
-    //cleanup().unwrap();
+    display_menu(myserver).unwrap();
 }
 
-fn init(myserver: NodeConnection) {
-    println!(
-        "\nAttempting to connect to Zebrad @ {}:{}\n.\n.\n.",
-        myserver.url, myserver.port
-    );
-
-    let mymethod = "getinfo";
-    let body_string = format!(
-        "{{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"{}\", \"params\": []}}",
-        mymethod
-    );
-    let mut body = body_string.as_bytes();
-
-    let local_connection = myserver.url + ":" + &myserver.port.to_string();
-
-    let mut easy = Easy::new();
-    easy.url(&local_connection).unwrap();
-    easy.post(true).unwrap();
-    easy.post_field_size(body.len() as u64).unwrap();
-
-    // Set the Content-Type header to application/json
-    let mut list = curl::easy::List::new();
-    list.append("Content-Type: application/json").unwrap();
-    easy.http_headers(list).unwrap();
-
-    let mut data = Vec::new();
-    {
-        // Create transfer in separate scope ...
-        let mut transfer = easy.transfer();
-
-        // Request body
-        transfer
-            .read_function(|buf| Ok(body.read(buf).unwrap_or(0)))
-            .unwrap();
-
-        // Response body
-        transfer
-            .write_function(|new_data| {
-                data.extend_from_slice(new_data);
-                Ok(new_data.len())
-            })
-            .unwrap();
-
-        transfer.perform().expect("Could not connect!\n")
-        // .. to force drop it here, so we can use easy.response_code()
-    }
-
-    let check = easy.response_code().unwrap().to_string();
-
-    if check == "200" {
-        println!("Connected!");
-    } else {
-        println!("No response!");
-    }
-}
-fn display_menu() -> Result<()> {
+fn display_menu(myserver: String) -> Result<()> {
     let mymenu = "\nRusty-Zechub";
 
     let opts = [
@@ -127,51 +135,51 @@ fn display_menu() -> Result<()> {
     match choice {
         0 => {
             clear_terminal_screen();
-            display_mnemonic().unwrap();
+            display_mnemonic(myserver).unwrap();
         }
         1 => {
             clear_terminal_screen();
-            visualize_mempool().unwrap();
+            visualize_mempool(myserver).unwrap();
         }
         2 => {
             clear_terminal_screen();
-            getblockchaininfo(false);
+            getblockchaininfo(myserver, false);
         }
         3 => {
             clear_terminal_screen();
-            deserialize().unwrap();
+            deserialize(myserver).unwrap();
         }
         4 => {
             clear_terminal_screen();
-            //let mytxid = "e6726f9e4f4c627cd0f2efb5cf13e0a9537c17e13290d0e0cd828a60676c6183";
+
             println!("Enter your txid:\n");
             let mut input: String = String::new(); // Create a string variable
             std::io::stdin() // Get the standard input stream
-            .read_line(&mut input) // The read_line function reads data until it reaches a '\n' character
-            .expect("Unable to read Stdin"); // In case the read operation fails, it panics with the given message
-        
-            tx_details(&input.trim().to_string()).unwrap();
-        
+                .read_line(&mut input) // The read_line function reads data until it reaches a '\n' character
+                .expect("Unable to read Stdin"); // In case the read operation fails, it panics with the given message
+
+            tx_details(myserver, &input.trim().to_string()).unwrap();
         }
         5 => {
             clear_terminal_screen();
 
             //let match_result = command!().arg( Arg::new(input.as_mut_str())).get_matches();
-            //let block = match_result.get_one::<String>("block").unwrap(); //"1000";
-          
+            //let block = match_result.get_one::<String>("block").unwrap();
+
             println!("Enter your block:\n");
             let mut input: String = String::new(); // Create a string variable
             std::io::stdin() // Get the standard input stream
-            .read_line(&mut input) // The read_line function reads data until it reaches a '\n' character
-            .expect("Unable to read Stdin"); // In case the read operation fails, it panics with the given message
+                .read_line(&mut input) // The read_line function reads data until it reaches a '\n' character
+                .expect("Unable to read Stdin"); // In case the read operation fails, it panics with the given message
 
-            getblock(&input.trim().to_string()).unwrap();
+            getblock(myserver, &input.trim().to_string()).unwrap();
         }
         6 => {
             clear_terminal_screen();
-            getpeerinfo().unwrap();
+            getpeerinfo(myserver).unwrap();
         }
         7 => {
+            clear_terminal_screen();
             cleanup().unwrap();
             process::exit(1);
         }
@@ -182,21 +190,21 @@ fn display_menu() -> Result<()> {
 
     Ok(())
 }
-fn display_mnemonic() -> Result<()> {
+fn display_mnemonic(myaddress: String) -> Result<()> {
     let my_mnemonic: Mnemonic<English> = Mnemonic::generate(Count::Words24);
     let mn_str = my_mnemonic.into_phrase();
     println!("Phrase: {}\n", mn_str);
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
     Ok(())
 }
-fn visualize_mempool() -> Result<()> {
+fn visualize_mempool(myaddress: String) -> Result<()> {
     let mymethod = "getrawmempool";
     let mut body =
         r#"{"jsonrpc":"1.0", "id": "curltest", "method":"getrawmempool","params":[true]}"#
             .as_bytes();
 
     let mut easy = Easy::new();
-    easy.url("http://127.0.0.1:8232").unwrap();
+    easy.url(&myaddress).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(body.len() as u64).unwrap();
 
@@ -278,10 +286,10 @@ fn visualize_mempool() -> Result<()> {
     }
     */
 
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
     Ok(())
 }
-fn getblockchaininfo(no_output: bool) {
+fn getblockchaininfo(myaddress: String, no_output: bool) {
     /*
 
     curl -s --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: application/json' http://127.0.0.1:8232/
@@ -293,7 +301,7 @@ fn getblockchaininfo(no_output: bool) {
             .as_bytes();
 
     let mut easy = Easy::new();
-    easy.url("http://127.0.0.1:8232").unwrap();
+    easy.url(&myaddress).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(body.len() as u64).unwrap();
 
@@ -367,14 +375,12 @@ fn getblockchaininfo(no_output: bool) {
     println!("\n");
 
     if no_output {
-
     } else {
-        display_menu().unwrap();
+        display_menu(myaddress).unwrap();
     }
 }
-fn deserialize() -> Result<()> {
-
-    getblockchaininfo(true);
+fn deserialize(myaddress: String) -> Result<()> {
+    getblockchaininfo(myaddress.clone(), true);
 
     let file_path = "new.json";
     //let my_json = input.clone();
@@ -402,10 +408,10 @@ fn deserialize() -> Result<()> {
 
     println!("--------------------------------------------------");
     println!("Shielded Supply             | {:#?}\n", shielded_supply);
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
     Ok(())
 }
-fn tx_details(txid: &str) -> Result<()> {
+fn tx_details(myaddress: String, txid: &str) -> Result<()> {
     let mymethod = "getrawtransaction";
     let body_string = format!(
         "{{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"{}\", \"params\": [\"{}\",1]}}",
@@ -414,7 +420,7 @@ fn tx_details(txid: &str) -> Result<()> {
     let mut body = body_string.as_bytes();
 
     let mut easy = Easy::new();
-    easy.url("http://127.0.0.1:8232").unwrap();
+    easy.url(&myaddress).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(body.len() as u64).unwrap();
 
@@ -483,12 +489,10 @@ fn tx_details(txid: &str) -> Result<()> {
     // Create a new file with result
     newfile.write_all(buffer.as_bytes()).unwrap();
     println!("\n");
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
     Ok(())
 }
-fn getblock(block: &str) -> Result<()> {
-
-    
+fn getblock(myaddress: String, block: &str) -> Result<()> {
     let mymethod = "getblock";
     let body_string = format!(
         "{{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"{}\", \"params\": [\"{}\",2]}}",
@@ -499,7 +503,7 @@ fn getblock(block: &str) -> Result<()> {
     let mut body = body_string.as_bytes();
 
     let mut easy = Easy::new();
-    easy.url("http://127.0.0.1:8232").unwrap();
+    easy.url(&myaddress).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(body.len() as u64).unwrap();
 
@@ -568,10 +572,10 @@ fn getblock(block: &str) -> Result<()> {
     // Create a new file with result
     newfile.write_all(buffer.as_bytes()).unwrap();
     println!("\n");
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
     Ok(())
 }
-fn getpeerinfo () -> Result<()> {
+fn getpeerinfo(myaddress: String) -> Result<()> {
     let mymethod = "getpeerinfo";
     let body_string = format!(
         "{{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"{}\", \"params\": []}}",
@@ -580,7 +584,7 @@ fn getpeerinfo () -> Result<()> {
     let mut body = body_string.as_bytes();
 
     let mut easy = Easy::new();
-    easy.url("http://127.0.0.1:8232").unwrap();
+    easy.url(&myaddress).unwrap();
     easy.post(true).unwrap();
     easy.post_field_size(body.len() as u64).unwrap();
 
@@ -649,55 +653,65 @@ fn getpeerinfo () -> Result<()> {
     // Create a new file with result
     newfile.write_all(buffer.as_bytes()).unwrap();
     println!("\n");
-    
+
     let file_path = "peer_new.json";
-    let my_json: String = std::fs::read_to_string(file_path).expect("Couldn't find or load that file.");
-   
+    let my_json: String =
+        std::fs::read_to_string(file_path).expect("Couldn't find or load that file.");
+
     println!("Current peer count: {:?}\n", my_json.lines().count());
 
-    display_menu().unwrap();
+    display_menu(myaddress).unwrap();
 
     Ok(())
 }
 fn cleanup() -> std::io::Result<()> {
-
     if Path::new("output.json").exists() {
         std::fs::remove_file("output.json")?;
-    }else {};
+    } else {
+    };
 
     if Path::new("new.json").exists() {
         std::fs::remove_file("new.json")?;
-    }else {};
+    } else {
+    };
 
     if Path::new("mempool_new.json").exists() {
         std::fs::remove_file("mempool_new.json")?;
-    }else {};
+    } else {
+    };
 
     if Path::new("mempool_output.json").exists() {
         std::fs::remove_file("mempool_output.json")?;
-    }else {};
+    } else {
+    };
 
     if Path::new("txid_new.json").exists() {
         std::fs::remove_file("txid_new.json")?;
-    }else {};
+    } else {
+    };
 
     if Path::new("txid_output.json").exists() {
         std::fs::remove_file("txid_output.json")?;
-    }else {};
+    } else {
+    };
 
-      if Path::new("block_new.json").exists() {
+    if Path::new("block_new.json").exists() {
         std::fs::remove_file("block_new.json")?;
-    }else {};
+    } else {
+    };
 
-      if Path::new("block_output.json").exists() {
+    if Path::new("block_output.json").exists() {
         std::fs::remove_file("block_output.json")?;
-    }else {};
-      if Path::new("peer_output.json").exists() {
+    } else {
+    };
+    if Path::new("peer_output.json").exists() {
         std::fs::remove_file("peer_output.json")?;
-    }else {};
-      if Path::new("peer_new.json").exists() {
+    } else {
+    };
+    if Path::new("peer_new.json").exists() {
         std::fs::remove_file("peer_new.json")?;
-    }else {};
+    } else {
+    };
 
     Ok(())
 }
